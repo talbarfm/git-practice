411e3c9ce5eb81c542efeab52c7f66086d1f1824
Tal Bar <talbarfm@gmail.com>
2022-03-03 08:23:47 +0200

    Edited sum67 - Changed the documentation, changes 6-7 to 5-6 and added some printing

--- python_program.py
+++ python_program.py
@@ -1,17 +1,20 @@
 def sum67(nums):
     """
     Gets the sum of the numbers in the array, except ignore sections of numbers starting with a 6
-    and extending to the next 7
+    and extending to the next 7. Some new documentation here as well
      param: nums(list): A list of numbers.
     return: Number of sum
     """
     new_list = []
-    while 6 in nums:
-        index_six = nums.index(6)
+    while 5 in nums:
+        index_six = nums.index(5)
         new_list.extend(nums[:index_six])
+        print("Im a change that's going to conflict")
         nums = nums[index_six:]
         nums = nums[nums.index(7) + 1:]
+        print("Me too")
     new_list.extend(nums)
+    print("Me three")
     return sum(new_list)
 
 
