41bbc7127aa77f8bd01d0065e1f7e5e30b547ffc
Tal Bar <talbarfm@gmail.com>
2022-03-03 08:26:05 +0200

    Edited sum67 - Changed the documentation, changes 6-7 to 4-6 and added some other printing

--- python_program.py
+++ python_program.py
@@ -1,23 +1,26 @@
-def sum67(nums):
+def sum47(nums):
     """
     Gets the sum of the numbers in the array, except ignore sections of numbers starting with a 6
-    and extending to the next 7
+    and extending to the next 7. Im another documentation! im going to conflict!
      param: nums(list): A list of numbers.
     return: Number of sum
     """
     new_list = []
-    while 6 in nums:
-        index_six = nums.index(6)
+    # Im an added comment
+    while 4 in nums:
+        index_six = nums.index(4)
         new_list.extend(nums[:index_six])
+        print("I liked conflicting with other code")
         nums = nums[index_six:]
         nums = nums[nums.index(7) + 1:]
+    print("CONFLICTS!")
     new_list.extend(nums)
     return sum(new_list)
 
 
 def main():
-    print(sum67([1, 2, 3, 4, 5, 6, 7, 2, 3, 6, 2, 3, 7, 0, 1]))
-    print(sum67([]))
+    print(sum47([1, 2, 3, 4, 5, 6, 7, 2, 3, 6, 2, 3, 7, 0, 1]))
+    print(sum47([]))
 
 
 if __name__ == "__main__":
